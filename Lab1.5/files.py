91fb2bd6d4a6f9c454d40806e249658ad12eeb3a
Александр Б <weird1982@gmail.com>
2021-03-19 16:40:03 +0300

    Lab1.5 updated

--- Lab1.5/files.py
+++ Lab1.5/files.py
@@ -5,9 +5,13 @@ ipList = []
 for name in glob.glob("./config_files/*.txt"):
     with open(name) as f:
         for line in f:
-            if line.find('IP address') > 0:
-                ip = line.split(":")[1].strip()
-                if ip not in ipList:
-                    ipList.append(ip)
-
+            if line.find('ip address ') != -1:
+                ip = line.split("address")[1].strip()
+                try:
+                    int(ip[0])
+                    if ip not in ipList:
+                        ipList.append(ip)
+                except:
+                    pass
 print(ipList)
+print(len(ipList))
